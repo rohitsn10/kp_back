0e6ed66c8299ba1eb414843470b21ee7703cad83
Parth Darji <interns@n10tech.com>
2025-01-30 12:31:35 +0530

    material update delete

--- configure/material_management/urls.py
+++ configure/material_management/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('material_management_create', MaterialManagementCreateViewSet.as_view({'post':'create','get':'list'}), name='material_management_create'),
+    path('material_management_update/<int:material_id>', MaterialManagementUpdateViewSet.as_view({'post':'update','delete':'destroy'}), name='material_management_update'),
 
 ]
