231d3947f11af14e3970441797fb85772307bd0e
Parth Darji <interns@n10tech.com>
2025-01-31 09:57:21 +0530

    url change land category update

--- configure/land_module/urls.py
+++ configure/land_module/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('land_category_create', CreateLandCategoryViewSet.as_view({'post':'create','get':'list'}), name='land_category'),
-    path('land_category_update/<int:land_category_id>', UpdateLandCategoryViewSet.as_view({'put':'update','get':'list'}), name='land_category_update'),
+    path('land_category_update/<int:land_category_id>', UpdateLandCategoryViewSet.as_view({'put':'update','delete':'destroy'}), name='land_category_update'),
     
     path('create_land_bank_master', LandBankMasterCreateViewset.as_view({'post':'create','get':'list'}), name='land_bank'),
     path('update_land_bank_master/<int:land_bank_id>', LandBankMasterUpdateViewset.as_view({'put':'update','delete':'destroy'}), name='land_bank_update'),
