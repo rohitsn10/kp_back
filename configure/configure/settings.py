0e71eb85b42eefbf49bed01d6b6d4eef62b52059
internsn10 <interns@n10tech.com>
2025-05-22 21:46:45 +0530

    cors error

--- configure/configure/settings.py
+++ configure/configure/settings.py
@@ -38,9 +38,12 @@ CORS_ALLOW_CREDENTIALS = True
 # ]
 CORS_ALLOW_ALL_ORIGINS = True 
 
+CSRF_TRUSTED_ORIGINS = ["http://13.53.78.89"]
+
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -64,6 +67,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
