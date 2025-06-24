1015233821dd233bef04420eabb721cb528eb25c
internsn10 <interns@n10tech.com>
2025-06-24 22:43:49 +0530

    weasyprint

--- configure/configure/urls.py
+++ configure/configure/urls.py
@@ -19,7 +19,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
+from django.urls import re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user_profile/',include('user_profile.urls')),
@@ -34,3 +36,8 @@ urlpatterns = [
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += [
+    re_path(r'^.*$', never_cache(TemplateView.as_view(template_name="index.html"))),
+]
